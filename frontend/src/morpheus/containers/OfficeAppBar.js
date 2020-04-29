dbaada9ebe9894ec8400d0b24c756b31d4802ace
Lucas Nichele <lucas.nichele@platform-software.com>
2020-04-28 21:07:32 -0300

    changed footer and title

--- frontend/src/morpheus/containers/OfficeAppBar.js
+++ frontend/src/morpheus/containers/OfficeAppBar.js
@@ -32,7 +32,7 @@ const OfficeAppBar = ({
   currentUser
 }) => (
   <>
-    <AppBarTitle>Matrix</AppBarTitle>
+    <AppBarTitle>Amplação</AppBarTitle>
     <MenuOffice
       filter={officeFilter}
       onChangeFilter={onChangeOfficeFilter}
